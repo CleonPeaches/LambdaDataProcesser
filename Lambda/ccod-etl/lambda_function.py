ecf5bdb575f8ec77b21c74e411ba020e03bd7150
Drew Engberson <drew.engberson@denvergov.org>
2020-04-02 13:10:44 -0600

    Create exile objects and delete objects from staging functions.

--- Lambda/ccod-etl/lambda_function.py
+++ Lambda/ccod-etl/lambda_function.py
@@ -9,19 +9,16 @@ from time import gmtime, strftime, sleep
 
 s3_resource = boto3.resource('s3')
 s3_client = boto3.client('s3')
-glue_client = boto3.client('glue')
 ssm_client = boto3.client('ssm')
 
 def get_resources(event):
     source_key = str(event['Records'][0]['s3']['object']['key'])
-    dest_bucket_string = os.environ['destination_bucket']
     source_name = source_key.split('/')[-3]
     return {
-        'source_bucket_string': os.environ['source_bucket'],
+        'source_bucket': os.environ['source_bucket'],
         'source_key': source_key,
-        'destination_bucket': s3_resource.Bucket(dest_bucket_string),
-        'dest_bucket_string': dest_bucket_string,
-        'exile_bucket_string': os.environ['exile_bucket'],
+        'destination_bucket': os.environ['destination_bucket'],
+        'exile_bucket': os.environ['exile_bucket'],
         'source_name': source_name,
         'source_object_name': source_key.split('/')[-2],
         'column_partition': ['created_date'],
@@ -48,30 +45,45 @@ def try_get_tags(resources):
         tag_set = get_tags(resources['source_name'], resources['source_object_name'])
     except ValueError as e:
         if str(e) == 'This object has no corresponding tags.':
-            exile_object(resources)
+            delete_message = exile_object(resources)
+            print(delete_message)
         raise e
     else:
         return tag_set
 
 def exile_object(resources):
     copy_source = {
-        'Bucket': resources['source_bucket_string'],
+        'Bucket': resources['source_bucket'],
         'Key': resources['source_key']
     }
-    exile_bucket = s3_resource(resources['exile_bucket-string'])
+    exile_bucket = s3_resource.Bucket(resources['exile_bucket'])
     exile_bucket.copy(copy_source, resources['source_key'])
+    delete_message = delete_object_from_staging(resources)
+    return delete_message
 
+def delete_object_from_staging(resources):
+    source_bucket = s3_resource.Bucket(resource['source_bucket'])
+    delete_message = source_bucket.delete_objects(
+        Delete={
+            'Objects': [
+                {
+                    'Key': resources['source_key']
+                }
+            ]
+        }
+    )
+    return delete_message
     
-def convert_to_data_frame(source_bucket_string, source_key):
-    content_object = s3_resource.Object(source_bucket_string, source_key)
+def convert_to_data_frame(source_bucket, source_key):
+    content_object = s3_resource.Object(source_bucket, source_key)
     file_content = content_object.get()['Body'].read().decode('windows-1252').strip()
     json_content = json.loads(file_content)
     data_frame = pandas.json_normalize(json_content)
     data_frame['ingestion_timestamp'] = str(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
     return data_frame
     
-def get_path(source_name, object_name, dest_bucket_string):
-    path = ('s3://' + dest_bucket_string + '/' +
+def get_path(source_name, object_name, destination_bucket):
+    path = ('s3://' + destination_bucket + '/' +
                       source_name + '/' +
                       object_name + '/')
     return path
@@ -97,10 +109,10 @@ def tag_objects(bucket_string, prefix, tag_set):
 def lambda_handler(event, context):
     resources = get_resources(event)
     tag_set = try_get_tags(resources)
-    data_frame = convert_to_data_frame(resources['source_bucket_string'], resources['source_key'])
-    path = get_path(resources['source_name'], resources['source_object_name'], resources['dest_bucket_string'])
+    data_frame = convert_to_data_frame(resources['source_bucket'], resources['source_key'])
+    path = get_path(resources['source_name'], resources['source_object_name'], resources['destination_bucket'])
     compression_message = convert_to_parquet(data_frame, path, resources['column_partition'])
-    tag_messages = tag_objects(resources['dest_bucket_string'], resources['prefix'], tag_set)
+    tag_messages = tag_objects(resources['destination_bucket'], resources['prefix'], tag_set)
     return {
         'statusCode': 200,
         'body': json.dumps({
